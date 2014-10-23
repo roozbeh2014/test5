a7b74cfd5d0465663263be2d6fa089eb60b8fc99
roozbeh <roozbeh.manshaei@ryerson.ca>
2014-10-23 07:16:18 -0400

    Add new variable (c)

---
+++ src/test5.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : test5.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int a = 2;
+	int b = 7;
+	int c = 5;
+	cout << a + b + c;
+	return 0;
+
+}
